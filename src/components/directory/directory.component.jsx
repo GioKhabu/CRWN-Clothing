bd271a59f1484567ff7132d3ff509fc2292a5ea3
unknown <giokhabu@gamil.com>
2023-02-06 12:05:52 +0400

    created directory and category-item components

---
+++ src/components/directory/directory.component.jsx
@@ -0,0 +1,15 @@
+import Categoryitem from "../category-item/category-tiem.component";
+import "./directory.styles.scss";
+
+const Directory = ({categories}) => {
+  
+  return (
+    <div className="categories-container">
+      {categories.map((category) => {
+        return <Categoryitem key={category.id} category={category} />;
+      })}
+    </div>
+  );
+};
+
+export default Directory;
